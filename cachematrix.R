758727a4300233cfb8cd0a2d395f6308b8ffc14d
ziyakhan <ziyakhanzik@gmail.com>
2015-06-21 18:52:51 +0530

    Programming Assignment 2
    The assignment is about writing a function in R that takes a matrix and
    calculates its inverse, also saves the result in cache database so that
    if the same matrix passed through the function again, it directly
    fetches the result from the database and saves the computation time.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function( x = matrix())
+{    		
+		original_matrix <<- x
+		inverse_matrix <<- NULL
+		set_matrix = function(z)
+		{	
+			original_matrix <<- z
+			inverse_matrix <<- NULL
+		}
+		get_matrix = function()
+		{
+			original_matrix
+		}
+		set_inverse = function(y = matrix())
+		{
+			inverse_matrix <<- y
+		}	
+		get_inverse = function()
+		{
+			inverse_matrix
+		}		
+		list( set_matrix = set_matrix, get_matrix = get_matrix, set_inverse = set_inverse, get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...)
+{
+ 		if(!is.null(inverse_matrix))
+		{
+			message("Getting cached Inverse")
+		}
+		else
+		{	
+			message("Creating new Inverse")
+			inverse_matrix <<- solve(original_matrix)
+		}
+		inverse_matrix
 }
+		
+		
+
